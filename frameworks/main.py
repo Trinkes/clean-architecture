dd5a1af490e123fe2eb5320b1195d69c9d4c9d1b
trinkes <fabiojscosta1583@gmail.com>
2020-04-23 01:17:33 +0100

    minor refactor
    Signed-off-by: trinkes <fabiojscosta1583@gmail.com>

--- frameworks/main.py
+++ frameworks/main.py
@@ -3,9 +3,7 @@ from interface_adapter.api_response_mapper import ApiResponseMapper
 from interface_adapter.api_topic_repository import ApiTopicRepository
 from use_cases.topic_service import TopicService
 
-
-def find_related_topic(word: str) -> str:
-    service = TopicService(
-        ApiTopicRepository(RequestsTopicApi('http://api.duckduckgo.com/'),
-                           ApiResponseMapper()))
-    return service.find_related_topic(word).title
+service = TopicService(
+    ApiTopicRepository(RequestsTopicApi('http://api.duckduckgo.com/'),
+                       ApiResponseMapper()))
+print(service.find_related_topic('java').title)
--- main.py
+++
@@ -1,3 +0,0 @@
-from frameworks.example1 import find_related_topic
-
-print(find_related_topic('ball'))
